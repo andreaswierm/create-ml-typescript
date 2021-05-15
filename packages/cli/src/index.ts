43a03a99f928ffa61cc7fe9ad529105e5d24cee0
Andreas Wiermann Casas <andreas.w.dev@gmail.com>
2021-05-15 10:29:07 -0300

    started

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -1,9 +1,4 @@
+#!/usr/bin/env node
 // add another package with `yo lerna-typescript:package @myscope/my-new-package` instead of `lerna create`
 
-export function greet(name: string): string {
-  return `@create-ml-typescript/cli says: hello to ${name}`;
-}
-
-export function add(a: number, b: number): number {
-  return a + b;
-}
+console.log("hello from create-ml-typescript");
